75b13ce6b003098bb56844b0dbfac53c77ca16d8
nikhi <nikhi@DESKTOP-AO42BLN>
2020-02-07 12:23:08 -0500

    Add project files.

---
+++ OOPschools/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+
+using System;
+
+namespace OOPschools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            myObj.fullThrottle();
+            Console.WriteLine(myObj.color);
+            Console.WriteLine(myObj.maxSpeed);
+            Car Ford = new Car();
+            Ford.model = "Mustang";
+            Ford.color = "red";
+            Ford.year = 1969;
+
+            Car Opel = new Car();
+            Opel.model = "Astra";
+            Opel.color = "white";
+            Opel.year = 2005;
+
+            Console.WriteLine(Ford.model);
+            Console.WriteLine(Opel.model);
+            Console.WriteLine(Ford.year);
+        }
+    }
+
+    class Car
+    {
+        //public string color = "red";
+        public int maxSpeed = 200;
+        public string model;
+        public string color;
+        public int year;
+        public void fullThrottle()    // method
+        {
+            Console.WriteLine("The car is going as fast as it can!");
+        }
+    }
+
+}
+
+
+
